fe1905ab066fd0d57ae2a37c57b2c9d7135fbe44
Ashutosh kumar verma <ashutoshverma117@gmail.com>
2022-01-31 11:49:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import './App.css';
+import Jadoo from './Jadoo.png';
+import Nav1 from './Nav Item.png';
+import Nav2 from './Nav2.png';
+import Nav3 from './Nav3.png';
+import Nav4 from './Nav4.png';
+import login from './login.png';
+import signup from './Sign up.png';
+import rectangle from './rectangle.png';
+import en from './EN.png';
+import vector from './Vector 1.png';
+import traveller from './traveller.png';
+import tagline from './Tagline.png';
+import text from './Heading.png';
+import Decore1 from './Decore1.png';
+import satelite from './satellite-dish.png';
+import group51 from './Group 51.png';
+import group50 from './Group 50.png';
+import group49 from './Group 49.png';
+import playdemo from './Play Demo.png';
+import destination from './Destination.png';
+import destination2 from './Destination2.png';
+import destination3 from './Destination3.png';
+import image from './Image.png';
+import group7 from './Group 7.png';
+import group11 from './Group 11.png';
+import group12 from './Group 12.png';
+import rectangle164 from './Rectangle 164.png';
+import group76 from './Group 76.png';
+import navrow from './Nav Row.png';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Jadoo} className="Jadoo" alt="logo" />
+        <img src={Nav1} className="nav_item" alt="nav1" />
+        <img src={Nav2} className="nav2" alt="nav1" />
+        <img src={Nav3} className="nav3" alt="nav1" />
+        <img src={Nav4} className="nav4" alt="nav4" />
+        <img src={login} className="login" alt="login" />
+        <img src={signup} className="sign_up" alt="login" />
+        <img src={rectangle} className="rectangle" alt="rectangle" />
+        <img src={en} className="en" alt="en" />
+        <img src={vector} className="vector" alt="en" />
+        <img src={traveller} className="traveler" alt="en" />
+        <img src={tagline} className="tagline" alt="en"/>
+        <img src={text} className="text" alt="en" />
+        <img src={Decore1} className="Decore1" alt="en" />
+       
+
+        
+        <p className="text2">Built Wicket longer admire do barton vanity itself do in it
+        Preferred to sportsmen i engrossed listening.Park gate 
+        sell they west hard for the.</p>
+        <p className='SSS'>CATEGORY</p>
+        <p className='KKK'>We Offer Best Services</p>
+        <img src={satelite} className="satelite" alt="en" />
+        <p className="set">Calculated Weather</p>
+        <img src={group51} className="group51" alt="en" />
+        <p className="sit">Best Flights</p>
+        <img src={group50} className="group50" alt="en" />
+        <img src={group49} className="group49" alt="en" />
+        <button onclick="" className="button" >Find out more</button>
+        <a href="https://www.w3schools.com"><img src={playdemo} className="playdemo"  alt="en" /></a>
+       
+        <p className="fit">Local Events</p>
+        <p className="git">Customization</p>
+        <div className="kit">
+        <p>Built Wicket longer admire do barto vanity itself do in it.</p>
+        </div> 
+        <p className="hix">Engrossed listening. Park gate sell they west hard for the.</p>
+        <p className="let">Barton vanity itself do in it. Preferd to men it engrossed listening. </p>
+        <p className="ret">We deliver outsourced
+aviation services for
+military customers</p>
+<p className="met">Top Selling</p>
+<p className="kwr">Top Destinations</p>
+<img src={destination} className="destination" alt="en" />
+<img src={destination2} className="destination2" alt="en" />
+<img src={destination3} className="destination3" alt="nav4" />
+<p className="a">Easy and Fast</p>
+<p className="at">Book Your Next Trip In Three Steps</p>
+<img src={image} className="image" alt="nav4" />
+<img src={group7} className="group7" alt="nav4" />
+<img src={group11} className="group11" alt="nav4" />
+<img src={group12} className="group12" alt="nav4" />
+<p className="choose">Choose Destination</p>
+<p className="o">Lorem ipsum dolor sit amet, consectetur 
+adipiscing elit. Urna, tortor tempus. </p>
+<p className="make">Make Payment</p>
+<p className="u">Lorem ipsum dolor sit amet, consectetur 
+adipiscing elit. Urna, tortor tempus. </p>
+<p className="j">Reached Airport On Selected Date</p>
+<p className="l">Lorem ipsum dolor sit amet, consectetur 
+adipiscing elit. Urna, tortor tempus. </p>
+<img src={rectangle164} className="rectangle164" alt="nav4" />
+<p className="r">Subscribe to get information, latest news and other
+interesting offers about Cobham</p>
+<img src={group76} className="group76" alt="nav4" />
+<img src={navrow} className="navrow" alt="nav4" />
+
+        </header>
+    </div>
+  );
+}
+
+export default App;
